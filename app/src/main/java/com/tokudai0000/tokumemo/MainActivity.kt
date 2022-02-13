f8cc1c9207481f4f95c52c7f2ff30323b3a955af
Akihiro Matsuyama <a@art.jp>
2022-02-13 12:26:44 +0900

    webViewの実装

--- app/src/main/java/com/tokudai0000/tokumemo/MainActivity.kt
+++ app/src/main/java/com/tokudai0000/tokumemo/MainActivity.kt
@@ -2,10 +2,18 @@ package com.tokudai0000.tokumemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val webView = findViewById<WebView>(R.id.webView)
+        //ウェブページがクロム（または、その他の検索アプリ）に開かなくてアプリのwebviewに開かるような設定
+        webView.webViewClient = object : WebViewClient(){}
+        //webviewに入力された値に対するウェブページを表示
+        webView.loadUrl("https://www.tokushima-u.ac.jp/")
+
     }
 }
