bc802b119b4836ca251f47aa30d75510ce159270
Akihiro Matsuyama <a@art.jp>
2022-02-14 23:48:42 +0900

    メニュータップ判定、テスト実装

--- app/src/main/java/com/tokudai0000/tokumemo/menu/MenuActivity.kt
+++ app/src/main/java/com/tokudai0000/tokumemo/menu/MenuActivity.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.tokudai0000.tokumemo.R
 
@@ -28,29 +29,10 @@ class MenuActivity : AppCompatActivity() {
         return false
     }
 
-    fun onItemClick(parent: AdapterView<*>?, v: View, position: Int, id: Long) {
-//        DataManager.instance.isExecuteJavascript = true
-        val intent = Intent()
-        intent.putExtra("KEY_STRING", menuListsUrl[position])
-        setResult(RESULT_OK, intent)
-
-        // このアクティビティを終了する
-        finish()
-    }
-
     private fun setupListView() {
         listView = findViewById<ListView>(R.id.listView)
 
-        // simple_list_item_1 は、 もともと用意されている定義済みのレイアウトファイルのID
-        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, menuListsTitle)
-        // ListViewにadapterをセット
-        listView?.setAdapter(arrayAdapter)
-        // クリックリスナーをセット
-//        listView?.setOnItemClickListener(this)
-    }
-
-    companion object {
-        private val menuListsTitle = arrayOf(
+        val menuListsTitle = arrayOf(
                 "教務事務システム",
                 "マナバ",
                 "図書貸し出し延長",
@@ -58,14 +40,15 @@ class MenuActivity : AppCompatActivity() {
                 "このアプリについて",
                 "図書Web[常三島]"
         )
-        private val menuListsUrl = arrayOf(
-                "",
-                "",
-                "",
-                "パスワード",
-                "このアプリについて",
-                ""
-        )
+
+        // simple_list_item_1 は、 もともと用意されている定義済みのレイアウトファイルのID
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, menuListsTitle)
+        // ListViewにadapterをセット
+        listView?.adapter = arrayAdapter
+        // クイックリスナー
+        listView?.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this, menuListsTitle[position], Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
