2e853475a2a6b82a213bf708a6eb8e0b6df2a5e8
Akihiro Matsuyama <a@art.jp>
2022-02-18 00:46:50 +0900

    password画面作成

---
+++ app/src/main/java/com/tokudai0000/tokumemo/menu/password/PasswordActivity.kt
@@ -0,0 +1,15 @@
+package com.tokudai0000.tokumemo.menu.password
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tokudai0000.tokumemo.R
+
+class PasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password)
+
+    }
+
+}
