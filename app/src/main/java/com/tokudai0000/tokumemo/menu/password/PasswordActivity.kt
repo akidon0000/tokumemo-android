78a8343a234ef77c030eaa7799215df7d75a657c
Akihiro Matsuyama <a@art.jp>
2022-02-18 14:09:03 +0900

    データ保存の試作

--- app/src/main/java/com/tokudai0000/tokumemo/menu/password/PasswordActivity.kt
+++ app/src/main/java/com/tokudai0000/tokumemo/menu/password/PasswordActivity.kt
@@ -1,13 +1,14 @@
 package com.tokudai0000.tokumemo.menu.password
 
-import android.content.Intent
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
-import android.webkit.WebView
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.tokudai0000.tokumemo.R
-import com.tokudai0000.tokumemo.menu.MenuActivity
 
 class PasswordActivity : AppCompatActivity() {
 
@@ -15,11 +16,41 @@ class PasswordActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_password)
 
+        // "DataStore"という名前でインスタンスを生成
+        val dataStore: SharedPreferences = getSharedPreferences("DataStore", Context.MODE_PRIVATE)
+
+
         // Outlet
         val backButton = findViewById<Button>(R.id.backButton)
+        val registerButton = findViewById<Button>(R.id.registerButton)
+
+        val cAccountTextField = findViewById<EditText>(R.id.cAccountTextField)
+        val cAccountTextSizeLabel = findViewById<TextView>(R.id.cAccountTextSizeLabel)
+        val cAccountMessageLabel = findViewById<TextView>(R.id.cAccountMessageLabel)
+
+        val passwordTextField = findViewById<EditText>(R.id.passwordTextField)
+        val passwordTextSizeLabel = findViewById<TextView>(R.id.passwordTextSizeLabel)
+        val passwordMessageLabel = findViewById<TextView>(R.id.passwordMessageLabel)
 
         // Action
-        backButton.setOnClickListener { v: View? -> finish() }
+        backButton.setOnClickListener { v: View? ->
+//            finish()
+            // "Input"から読み出す
+            val str = dataStore.getString("Input", "NoData")
+
+            passwordMessageLabel.text = str
+        }
+
+        registerButton.setOnClickListener { v: View? ->
+//            cAccountTextField.text
+            // 文字列を"Input"に書き込む
+            val editor = dataStore.edit()
+            editor.putString("Input", "文字列")
+
+            //editor.commit();
+            editor.apply()
+        }
+
     }
 
 }
