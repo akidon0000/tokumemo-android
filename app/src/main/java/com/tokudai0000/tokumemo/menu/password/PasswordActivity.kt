3f2479193cfe1c151dff73f0289eea9047be839f
Akihiro Matsuyama <a@art.jp>
2022-02-18 14:33:20 +0900

    暗号化保存成功

--- app/src/main/java/com/tokudai0000/tokumemo/menu/password/PasswordActivity.kt
+++ app/src/main/java/com/tokudai0000/tokumemo/menu/password/PasswordActivity.kt
@@ -7,18 +7,24 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
 import com.tokudai0000.tokumemo.R
+import javax.crypto.EncryptedPrivateKeyInfo
 
 class PasswordActivity : AppCompatActivity() {
-
+    companion object {
+        const val PREF_NAME = "encrypted_prefs"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_password)
 
         // "DataStore"という名前でインスタンスを生成
         val dataStore: SharedPreferences = getSharedPreferences("DataStore", Context.MODE_PRIVATE)
-
+//        val dataStore1: EncryptedPrivateKeyInfoSharedPreferences = getSharedPreferences("DataStore", Context.MODE_PRIVATE)
 
         // Outlet
         val backButton = findViewById<Button>(R.id.backButton)
@@ -36,9 +42,30 @@ class PasswordActivity : AppCompatActivity() {
         backButton.setOnClickListener { v: View? ->
 //            finish()
             // "Input"から読み出す
-            val str = dataStore.getString("Input", "NoData")
+//            val str = dataStore.getString("Input", "NoData")
+//
+//            passwordMessageLabel.text = str
 
-            passwordMessageLabel.text = str
+            val mainKey = MasterKey.Builder(applicationContext)
+                    .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                    .build()
+
+            val prefs = EncryptedSharedPreferences.create(
+                    applicationContext,
+                    PREF_NAME,
+                    mainKey,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            )
+
+            with (prefs.edit()) {
+                putString("foo", "bar")
+                apply()
+            }
+
+            val savedValue = prefs.getString("foo", "Nothing")
+            Toast.makeText(this, """the saved value of the key "foo" is $savedValue""", Toast.LENGTH_LONG).show()
+
         }
 
         registerButton.setOnClickListener { v: View? ->
