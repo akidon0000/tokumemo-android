5dd2934f3925280de19ddb63d91619a50f96ae2f
Akihiro Matsuyama <a@art.jp>
2022-02-18 20:25:30 +0900

    menuひとまず完成

--- app/src/main/java/com/tokudai0000/tokumemo/View/Menu/MenuActivity.kt
+++ app/src/main/java/com/tokudai0000/tokumemo/View/Menu/MenuActivity.kt
@@ -11,39 +11,31 @@ import com.tokudai0000.tokumemo.R
 
 class MenuActivity : AppCompatActivity() {
 
-    private var listView: ListView? = null
-//    var delegate: MainActivity? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
-        setupListView()
+        // findViewById
+        var listView = findViewById<ListView>(R.id.listView)
+
+        // Action
+        listView?.setOnItemClickListener { parent, view, position, id ->
+            // 親(MainActivity)にどのセルがタップされたのかを伝える
+            val intent = Intent()
+            intent.putExtra("MenuID_KEY", Constant.menuLists[position].id.toString())
+            intent.putExtra("MenuUrl_KEY", Constant.menuLists[position].url)
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
+
+        // ListViewに表示する内容をセット
+        listView?.adapter = ListAdapter(this, Constant.menuLists)
     }
 
     // メニュー外をタップされた時、MainActivityに戻る
     override fun onTouchEvent(motionEvent: MotionEvent): Boolean {
         finish()
         return false
-    }
-
-    private fun setupListView() {
-        listView = findViewById<ListView>(R.id.listView)
-
-        //アダプターにユーザーリストを導入
-        val adapter = ListAdapter(this, Constant.menuLists)
-        // ListViewにadapterをセット
-        listView?.adapter = adapter
-        // クイックリスナー
-        listView?.setOnItemClickListener { parent, view, position, id ->
-            val intent = Intent()
-            intent.putExtra("MenuID_KEY", Constant.menuLists[position].id.toString())
-            intent.putExtra("MenuUrl_KEY", Constant.menuLists[position].url)
-            setResult(Activity.RESULT_OK, intent)
-//            delegate!!.test()
-
-            finish()
-        }
     }
 
 }
