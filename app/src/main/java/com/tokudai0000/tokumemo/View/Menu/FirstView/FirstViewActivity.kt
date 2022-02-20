b900a959f23e4e68a4c7032ab8130c027a0f7482
Akihiro Matsuyama <a@art.jp>
2022-02-20 18:29:32 +0900

    ファイル作成

---
+++ app/src/main/java/com/tokudai0000/tokumemo/View/Menu/FirstView/FirstViewActivity.kt
@@ -0,0 +1,14 @@
+package com.tokudai0000.tokumemo.View.Menu.FirstView
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tokudai0000.tokumemo.R
+
+class FirstViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_view)
+    }
+
+}
---
+++ app/src/main/java/com/tokudai0000/tokumemo/View/Menu/Syllabus/SyllabusActivity.kt
@@ -0,0 +1,14 @@
+package com.tokudai0000.tokumemo.View.Menu.Syllabus
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tokudai0000.tokumemo.R
+
+class SyllabusActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_syllabus)
+    }
+
+}
